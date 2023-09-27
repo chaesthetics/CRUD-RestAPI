a057a90404df664cd965c4566a2a2fd130469575
Auriel James <aurieljames11@gmail.com>
2023-09-28 06:26:51 +0800

    issue/#1 Tailwind Installed Successfully

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,15 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
+    <div class="max-w-7xl mx-auto">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <main class="max-w-7xl mx-auto min-h-screen">
+    <RouterView />
+  </main>
 </template>
 
