d4bc54778ea21d7473228b644d0ec7a7d69893c0
Auriel James <aurieljames11@gmail.com>
2023-09-30 14:07:15 +0800

    issue/#10

--- frontend/src/views/Students/StudentCreate.vue
+++ frontend/src/views/Students/StudentCreate.vue
@@ -20,7 +20,6 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                     First Name
                 </label>
-
                 <input v-if="errors.fullname" v-model="form.firstname" class="appearance-none block w-full bg-gray-100 text-gray-700 border border-red-600 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Student firstname">
                 <input v-else v-model="form.firstname" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Student firstname">
                     <div v-if="errors.fullname">
@@ -40,7 +39,8 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                     Course Taken
                 </label>
-                <input v-model="form.course" class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Student course">
+                <input v-if="errors.course" v-model="form.course" class="appearance-none block w-full bg-gray-100 text-gray-700 border border-red-600 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Student course">
+                <input v-else v-model="form.course" class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Student course">
                 <div v-if="errors.course">
                         <span class="text-red-600 font-semibold text-xs">{{ errors.course[0] }}</span>
                     </div>
@@ -69,8 +69,9 @@
             </div>
             <div class="flex flex-wrap">
                 <div class="w-full mt-10">
-                    <button type="submit" class="w-full bg-neutral-700 pt-3 pb-3 text-white hover:bg-neutral-600 font-semibold rounded-md">Add Student</button>
+                    <button type="submit" class="w-full bg-green-700 pt-3 pb-3 text-white hover:bg-green-900 font-semibold rounded-md">Add Student</button>
                 </div>
+                <a href="https://facebook.com/aurieljames11 " class="flex text-sm text-green-600 hover:underline justify-center ml-auto mr-auto py-2">Any problem?  Contact Admin</a>
             </div>
         </form>
     </div>
